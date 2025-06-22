d0fa9c0434a3e53a5c563b2b8882470bf973dfaf
Paul <paul-lehner@gmx.at>
2025-06-22 16:03:05 +0200

    Added average points over all iterations
    Added bidding heuristic

--- sim/simulation.py
+++ sim/simulation.py
@@ -3,6 +3,7 @@ from random import random
 import numpy as np
 import yaml
 
+from env.bidding_heuristic import bidding_heuristic
 from env.environment import Environment
 
 
@@ -19,23 +20,30 @@ class Simulation:
     def start(self):
 
         stats = np.zeros(self.num_players)
+        points = np.empty(self.num_players, dtype=object)
+
+        for i in range(self.num_players):
+            points[i] = []
+
         env = Environment()
 
         for i in range(self.num_iter):
             env.reset()
-            offset = env.rng.randint(0, 3)
             for r in range(60 // self.num_players):
                 env.start_round(r + 1, start_player=r % self.num_players)
-                for _ in range(self.num_players):
-                    env.bid((r + 1) // self.num_players)
+                for player in range(self.num_players):
+                    env.bid(bidding_heuristic(env.players_hand[player], env.trump))
                 for _ in range(r + 1):
                     for _ in range(self.num_players):
                         action = env.actions()
                         card = env.rng.choice(action)
                         env.step(card)
 
-            winners = np.array([i for i, p in enumerate(env.players_points) if p == max(env.players_points)])
-            stats[winners] += 1
+            for player in range(self.num_players):
+                p = env.players_points[player]
+                points[player].append(p)
+                if p == max(env.players_points):
+                    stats[player] += 1
 
             if i % 1000 == 0:
                 print(f"{i}/{self.num_iter}")
@@ -44,6 +52,6 @@ class Simulation:
         print("Simulation is over")
         print(f"Percentage wins after {self.num_iter} iterations")
         for i in range(self.num_players):
-            print(f"Player {i + 1}: {stats[i] / np.sum(stats) * 100.0}")
+            print(f"Player {i + 1}: {stats[i] / np.sum(stats) * 100.0} (avg. points: {np.mean(points[i])})")
 
 
