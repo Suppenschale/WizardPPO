e48404c2b921396c75bdef6904a5098fdb3d3644
Paul Lehner <paul-lehner@gmx.at>
2026-02-03 17:51:43 +0100

    change bidding heuristic to a discrete gaussian distribution

--- env/bidding_heuristic.py
+++ env/bidding_heuristic.py
@@ -1,27 +1,8 @@
-from env.card import Card
-from env.environment import JESTER
-from env.environment import WIZARD
+import numpy as np
 
 
-def bidding_heuristic(hand: list[Card], trump: Card) -> int:
-    bid = 0
-
-    for card in hand:
-
-        # For every Wizard in hand increase the bid
-        if card.rank == WIZARD:
-            bid += 1
-
-        # For every trump in hand increase the bid
-        if trump and card.suit == trump.suit and card.rank not in [JESTER, WIZARD]:
-            bid += 1
-
-        # For every 13 in hand increase the bid
-        if trump and card.rank == 13 and card.suit != trump.suit:
-            bid += 1
-
-        # In the last round (no trump), count all cards >= 10
-        if not trump and card.rank >= 10 and card.rank != WIZARD:
-            bid += 1
-
-    return min(max(bid, 0), len(hand)) #+ np.random.randint(0, 0)
+def bidding_heuristic(N: int, mu: float, alpha=0.09) -> int:
+    k = np.arange(N + 1)
+    probs = np.exp(-alpha * (k - mu) ** 2)
+    probs /= probs.sum()
+    return np.random.choice(k, p=probs).item()
