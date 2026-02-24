4aac10aad2a56b1a0944d447a6e989d41af453ef
Paul Lehner <paul-lehner@gmx.at>
2026-02-24 23:38:22 +0100

    update simulation.py

--- sim/simulation.py
+++ sim/simulation.py
@@ -30,69 +30,66 @@ class Simulation:
             self.STATE_BID_2 = state_mask[4]
             self.MASK_BID_2 = state_mask[5]
 
-    def start(self, num_round):
+    def start(self):
 
         stats = [0 for _ in range(self.num_players)]
         points = [[] for _ in range(self.num_players)]
-        bids = [[] for _ in range(self.num_players)]
-        tricks = [[] for _ in range(self.num_players)]
 
         for iteration in tqdm(range(self.iter), "Simulating"):
             env = Environment()
-            start = random.choice(range(env.num_players))
-            env.start_round(num_round, start)
 
             for p in range(env.num_players):
-                if iteration == 0:
-                    print()
-                    print(f"Player {env.cur_player}: ")
-                max_value = -10000000
-                max_bid = -1
-                for test_bid in range(env.num_rounds + 1):
-                    env.players_bid[env.cur_player] = test_bid
-                    state = env.get_state_vector()
-                    action_mask = env.get_action_mask()
-                    value, _ = self.policy(state, action_mask)
-                    if iteration == 0:
-                        print(f"Expected Future Value for bid {test_bid}: {value.item()}")
-                    if value > max_value:
-                        max_value = value
-                        max_bid = test_bid
-                if iteration == 0:
-                    print(f"Best bid : {max_bid} ({max_value.item()})")
-                    print()
-                bids[env.cur_player].append(max_bid)
-                env.bid(max_bid)
+                points[p].append(0)
 
-            for r in range(num_round):
+            for num_round in range(1, 16):
+                env.start_round(num_round, random.choice(range(env.num_players)))
 
-                for i in range(self.num_players):
-                    state = env.get_state_vector()
-                    action_mask = env.get_action_mask()
+                for p in range(env.num_players):
+                    max_value = -10000000
+                    max_bid = -1
+                    for test_bid in range(env.num_rounds + 1):
+                        env.players_bid[env.cur_player] = test_bid
+                        state = env.get_state_vector()
+                        action_mask = env.get_action_mask()
+                        value, _ = self.policy(state, action_mask)
+                        if value > max_value:
+                            max_value = value
+                            max_bid = test_bid
+                    env.bid(max_bid)
 
-                    #if iteration == 0 and r == 0:
-                    #    self.policies[i].print_special_state(0, self.STATE_BID_0, self.MASK_BID_0)
-                    #    self.policies[i].print_special_state(1, self.STATE_BID_1, self.MASK_BID_1)
-                    #    self.policies[i].print_special_state(2, self.STATE_BID_2, self.MASK_BID_2)
+                for r in range(num_round):
 
-                    action, _, _ = self.policy.select_action(state, action_mask)
-                    env.step(action)
+                    for i in range(self.num_players):
+                        state = env.get_state_vector()
+                        action_mask = env.get_action_mask()
+
+                        #if iteration == 0 and r == 0:
+                        #    self.policies[i].print_special_state(0, self.STATE_BID_0, self.MASK_BID_0)
+                        #    self.policies[i].print_special_state(1, self.STATE_BID_1, self.MASK_BID_1)
+                        #    self.policies[i].print_special_state(2, self.STATE_BID_2, self.MASK_BID_2)
+
+                        action, _, _ = self.policy.select_action(state, action_mask)
+                        env.step(action)
+
+                for player in range(self.num_players):
+                    p = env.players_points[player]
+                    points[player][-1] += p
 
             for player in range(self.num_players):
-                p = env.players_points[player]
-                tricks[player].append(env.players_tricks[player])
-                points[player].append(p)
-                if p == max(env.players_points):
+                final_points = [points[player][-1] for player in range(self.num_players)]
+
+                if points[player][-1] == max(final_points):
                     stats[player] += 1
 
         print()
         for p in range(self.num_players):
+
             print()
             print(f"Player {p + 1} :")
             for i in range(self.iter):
-                print(f"  Points = {points[p][i]} ({tricks[p][i]}/{bids[p][i]})")
+                print(f"  Points = {points[p][i]}")
 
-        winrate = [stats[i] / sum(stats) * 100.0 for i in range(self.num_players)]
+        winrate = [stats[i] / self.iter * 100.0 for i in range(self.num_players)]
         avg = [sum(points[i]) / len(points[i]) for i in range(self.num_players)]
 
         print("")
