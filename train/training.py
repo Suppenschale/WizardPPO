e75b3879eac5e510b70170520eef73f9a08f92f3
Paul Lehner <paul-lehner@gmx.at>
2026-02-25 00:59:52 +0100

    remove start logic to main.py

--- train/training.py
+++ train/training.py
@@ -285,38 +285,3 @@ class Training:
             print("Training interrupted")
         finally:
             torch.save(self.policy.state_dict(), os.path.join(self.path, "last_model.pth"))
-
-
-def start_tensorboard(path, port=6006):
-    tb = program.TensorBoard()
-    tb.configure(argv=[None, "--logdir", os.path.join(path, "board"), "--port", str(port)])
-    url = tb.launch()
-    print(f"TensorBoard running at {url}")
-
-
-def main():
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    path = os.path.join("log", timestamp)
-    os.makedirs(path, exist_ok=True)
-    os.makedirs(os.path.join(path, "board"), exist_ok=True)
-    print(f"Create {path}")
-    runPath = os.path.join(path, "board")
-    print(f"Create {runPath}")
-
-    with open("parameter.yaml", "r") as f:
-        config = yaml.safe_load(f)
-
-    lr = config["train"]["lr"]
-    iterations = config["train"]["iter"]
-
-    network = PPONetwork()
-    network.train()
-    optimizer = Adam(network.parameters(), lr=lr)
-
-    start_tensorboard(path)
-    train = Training(network, optimizer, path)
-    train.training_loop(iterations)
-
-
-if __name__ == "__main__":
-    main()
